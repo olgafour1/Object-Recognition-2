9a39315198ce5ff864db8dd97cc3453bfdaa795b
Unknown <marek.merten@est.fib.upc.edu>
2018-06-03 16:08:44 +0200

    parfor

--- code/check_distances.m
+++ code/check_distances.m
@@ -3,6 +3,13 @@ num_classes = 12;
 
 selected_class = 2;
 
+if false
+    c = parcluster('local');
+    c.NumWorkers = 8;
+    parpool(c, c.NumWorkers);
+end
+
+
 for i=1:length(samples)
     if labels(i) == selected_class
         model = samples{i};
@@ -44,22 +51,25 @@ if true
         end
     end
     
-    cls = selected_class
-    cls_count = length(class_indexes{cls})
-    distances1{cls} = zeros(cls_count, 1);
-    distances2{cls} = zeros(cls_count, 1);
-    dist1 = distances1{cls};
-    dist2 = distances2{cls};
-    cls_indexes = class_indexes{cls};
-    
-    parfor i=1:cls_count
-        dist1(i) = dtw(model, samples{cls_indexes(i)}, 0);
-        dist2(i) = dtw(model2, samples{cls_indexes(i)}, 0);
-    end
-    
-    distances1{cls} = dist1;
-    distances2{cls} = dist2;
+    %cls = selected_class
+    for cls=1:num_classes
+        cls_count = length(class_indexes{cls});
+        fprintf('class: %d  count: %d\n', cls, cls_count);
+        
+        distances1{cls} = zeros(cls_count, 1);
+        distances2{cls} = zeros(cls_count, 1);
+        dist1 = distances1{cls};
+        dist2 = distances2{cls};
+        cls_indexes = class_indexes{cls};
 
+        parfor i=1:cls_count
+            dist1(i) = dtw(model, samples{cls_indexes(i)}, 0);
+            dist2(i) = dtw(model2, samples{cls_indexes(i)}, 0);
+        end
+
+        distances1{cls} = dist1;
+        distances2{cls} = dist2;
+    end
 end
 
 mean_distances = [];
@@ -80,11 +90,14 @@ end
 
 
 figure;
-i = selected_class;
+%i = selected_class;
 %size(distances1{i})
 %size(distances2{i})
 
-scatter(distances1{i}, distances2{i});
+for i = 1:num_classes
+    scatter(distances1{i}, distances2{i});
+    hold on;
+end
 
 ylim([0 1000]);
 xlim([0 1000]);
